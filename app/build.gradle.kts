f6c5c99218e9930720d8d1f680d5259d18b32e0e
Ruben Renart <rubenrenart@gmail.com>
2024-11-02 03:02:09 +0100

    v1.0 - Lógica de la ventana principal creada. App Principal Terminada

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.appextra2_dado"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.appextra2_dado"
--- app/src/main/java/com/example/appextra2_dado/MainActivity.kt
+++ app/src/main/java/com/example/appextra2_dado/MainActivity.kt
@@ -1,23 +1,63 @@
 package com.example.appextra2_dado
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var tvResultado:TextView
+    private lateinit var btnDado:AppCompatButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-
+        initComponents()
+        initListeners()
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    private fun initComponents()
+    {
+        tvResultado = findViewById(R.id.tv_resultado)
+        btnDado = findViewById(R.id.btn_dado)
+    }
+
+    private fun initListeners()
+    {
+        btnDado.setOnClickListener()
+        {
+            Toast.makeText(this, "¡Dado tirado!", Toast.LENGTH_SHORT).show()
+            tirarDado()
+        }
+    }
+
+    private fun tirarDado()
+    {
+        val dado = Dado(6)
+        val tirada = dado.tirada()
+
+        tvResultado.text = tirada.toString()
+    }
+}
+
+class Dado(private val numLados:Int)
+{
+    fun tirada():Int
+    {
+        return (1..numLados).random()
     }
 }
