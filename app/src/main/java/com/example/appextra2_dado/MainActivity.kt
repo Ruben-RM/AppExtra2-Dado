9d887bdab7479bee579af1809921fc27100b7ddb
Ruben Renart <rubenrenart@gmail.com>
2024-11-02 03:26:10 +0100

    v2.0 - Lógica de la ventana principal funcionando. Mejora de la APP completada

--- app/src/main/java/com/example/appextra2_dado/MainActivity.kt
+++ app/src/main/java/com/example/appextra2_dado/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.appextra2_dado
 
 import android.annotation.SuppressLint
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.widget.ImageView
 import android.widget.TextView
@@ -23,6 +24,7 @@ class MainActivity : AppCompatActivity() {
 
         initComponents()
         initListeners()
+        initUI()
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -46,10 +48,27 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun initUI()
+    {
+        tirarDado()
+    }
+
     private fun tirarDado()
     {
         val dado = Dado(6)
         val tirada = dado.tirada()
+
+        val imagenDado = when (tirada) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        ivResultado.setImageResource(imagenDado)
+        ivResultado.contentDescription = tirada.toString()
     }
 }
 
